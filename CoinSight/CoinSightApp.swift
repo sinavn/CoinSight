7125f74fafcd3cb79eb2d598dffcca0931fb08ae
sinavn <vosogh858sina@gmail.com>
2023-11-10 17:11:00 +0330

    Initial Commit

---
+++ CoinSight/CoinSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinSightApp.swift
+//  CoinSight
+//
+//  Created by SinaVN on 8/19/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct CoinSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinSight/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CoinSight
+//
+//  Created by SinaVN on 8/19/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
